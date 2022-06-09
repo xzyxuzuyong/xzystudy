07619d276bdd3c40a180c1c99a7b412dd55a6f62
zuyong.xu <xzyxuzuyong@163.com>
2022-06-09 10:21:21 +0800

    commit files

---
+++ springboot-study001/src/main/java/com/example/springbootstudy001/SpringbootStudy001Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudy001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudy001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudy001Application.class, args);
+    }
+
+}
---
+++ springboot-study001/src/test/java/com/example/springbootstudy001/SpringbootStudy001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootstudy001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudy001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
